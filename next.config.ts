90f4695b6ccb28dce5bf5470c5cc5123c8352dc3
Steve Arkan Fitrano <stevelewiss2003@gmail.com>
2026-09-21 22:25:38 +0700

    Add opening video and event photo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hd339l09uzcdhf0y.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/interlude-image.png",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
