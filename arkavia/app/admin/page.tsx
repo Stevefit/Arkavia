8433a05ada9b9123ba68e59361fc361826cb4fec
Stevefit <100519604+Stevefit@users.noreply.github.com>
2026-09-21 08:00:45 +0700

    Delete arkavia directory

--- arkavia/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { COOKIE, validSession } from "@/lib/auth";
-import Dashboard from "@/components/Dashboard";
-export default async function Page() {
-  if (!(await validSession((await cookies()).get(COOKIE)?.value)))
-    redirect("/admin/login");
-  return <Dashboard />;
-}
--- arkavia/app/page.tsx
+++
@@ -1,4 +0,0 @@
-import Public from "@/components/Public";
-export default function Page() {
-  return <Public />;
-}
